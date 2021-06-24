dbe9f55f21b9b0d4a6141f566e4b692c839447f2
Martin Folkers <hello@twobrain.io>
2021-06-24 14:12:56 +0200

    Add page & field methods, give full control over cache config

--- index.php
+++ index.php
@@ -12,37 +12,60 @@
 
 @include_once __DIR__ . '/vendor/autoload.php';
 
-use Kirby\Http\Server;
-use Kirby\Toolkit\Dir;
-
 use S1SYPHOS\DejureOnline;
+
+
+function dejurify(string $text): string
+{
+    # Leave text unmodified if plugin is disabled (default)
+    if (!option('kirby3-dejure.enabled', false)) {
+        return $text;
+    }
+
+    # Ensure that path to cache directory exists
+    $cacheDir = kirby()->root('cache') . '/dejure-online.org/';
+
+    # Initialize DJO instance
+    $object = new DejureOnline($cacheDir,
+        option('kirby3-dejure.driver', 'file'),
+        option('kirby3-dejure.caching')
+    );
+
+    # Set defaults
+    $object->setProvider(option('kirby3-dejure.provider', Kirby\Http\Server::host()));
+    $object->setMail(option('kirby3-dejure.mail', ''));
+    $object->setLinkStyle(option('kirby3-dejure.linkStyle', 'weit'));
+    $object->setTarget(option('kirby3-dejure.target', '_blank'));
+    $object->setClass(option('kirby3-dejure.class', ''));
+    $object->setBuzer(option('kirby3-dejure.buzer', true));
+    $object->setCacheDuration(option('kirby3-dejure.cachePeriod', 2));
+    $object->setTimeout(option('kirby3-dejure.timeout', 3));
+
+    return $object->dejurify($text);
+}
 
 
 Kirby::plugin('s1syphos/kirby3-dejure', [
     'hooks' => [
-        'kirbytext:after' => function ($text) {
-            # Ensure that path to cache directory exists
-            $cacheDir = kirby()->root('cache') . '/dejure-online.org/';
-
-            if (!Dir::exists($cacheDir)) {
-                Dir::make($cacheDir);
+        'kirbytext:after' => function (string $text): string
+        {
+            return dejurify($text);
+        }
+    ],
+    'pageMethods' => [
+        'dejurify' => function (string $text): string
+        {
+            return dejurify($text);
+        }
+    ],
+    'fieldMethods' => [
+        'dejurify' => function (Kirby\Cms\Field $field, bool $useKirbytext = true): string
+        {
+            if ($useKirbytext === true) {
+                return dejurify($field->kt());
             }
 
-            # Initialize DJO instance
-            $object = new DejureOnline();
-
-            # Set defaults
-            $object->setCacheDir(option('kirby3-dejure.cacheDir', $cacheDir));
-            $object->setProvider(option('kirby3-dejure.provider', Server::host()));
-            $object->setMail(option('kirby3-dejure.mail', ''));
-            $object->setLinkStyle(option('kirby3-dejure.linkStyle', 'weit'));
-            $object->setTarget(option('kirby3-dejure.target', '_blank'));
-            $object->setClass(option('kirby3-dejure.class', ''));
-            $object->setBuzer(option('kirby3-dejure.buzer', true));
-            $object->setCachePeriod(option('kirby3-dejure.cachePeriod', 2));
-            $object->setTimeout(option('kirby3-dejure.timeout', 3));
-
-            return $object->dejurify($text);
+            return dejurify($field);
         }
     ],
 ]);
