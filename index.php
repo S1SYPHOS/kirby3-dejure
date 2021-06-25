7d9e3ed5f6904bf6dfddacdd9c8787079ede6810
Martin Folkers <hello@twobrain.io>
2021-06-25 09:29:37 +0200

    Implement new query parameters
    * Implement new parameters 'tooltip', 'lineBreak' & 'ignore'
    * Rework setters
    * Move init to function & add method for cache resets
    * Add phpDoc descriptions
    * Change 'buzer' default to 'false'
    * Add some trailing commas

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
  * @package   Kirby CMS
  * @author    S1SYPHOS <hello@twobrain.io>
  * @link      http://twobrain.io
- * @version   1.1.0
+ * @version   1.2.0
  * @license   MIT
  */
 
@@ -15,33 +15,77 @@
 use S1SYPHOS\DejureOnline;
 
 
-function dejurify(string $text): string
+/**
+ * Initializes new DejureOnline (DJO) instance
+ *
+ * @return \S1SYPHOS\DejureOnline
+ */
+function dejureInit(): \S1SYPHOS\DejureOnline
+{
+    # Ensure that path to cache directory exists
+    $cacheDir = kirby()->root('cache') . '/dejure-online.org/';
+
+    # Initialize DJO instance
+    return new DejureOnline(
+        option('kirby3-dejure.driver', 'file'),
+        option('kirby3-dejure.caching', ['storage' => $cacheDir])
+    );
+}
+
+
+/**
+ * Processes linkable citations & caches text (if uncached or expired)
+ *
+ * @param string $text Original (unprocessed) text
+ * @param string $ignore Judicial file numbers to be ignored
+ * @return string Processed text if successful, otherwise unprocessed text
+ */
+function dejurify(string $text, string $ignore = ''): string
 {
     # Leave text unmodified if plugin is disabled (default)
     if (!option('kirby3-dejure.enabled', false)) {
         return $text;
     }
 
-    # Ensure that path to cache directory exists
-    $cacheDir = kirby()->root('cache') . '/dejure-online.org/';
-
-    # Initialize DJO instance
-    $object = new DejureOnline($cacheDir,
-        option('kirby3-dejure.driver', 'file'),
-        option('kirby3-dejure.caching')
-    );
+    # Create DJO instance
+    $object = dejureInit();
 
     # Set defaults
-    $object->setProvider(option('kirby3-dejure.provider', Kirby\Http\Server::host()));
-    $object->setMail(option('kirby3-dejure.mail', ''));
+    # (1) General information
+    $object->setEmail(option('kirby3-dejure.mail', ''));
+
+    # (2) Text processing
+    $object->setBuzer(option('kirby3-dejure.buzer', false));
+    $object->setClass(option('kirby3-dejure.class', ''));
+    $object->setLineBreak(option('kirby3-dejure.lineBreak', 'auto'));
     $object->setLinkStyle(option('kirby3-dejure.linkStyle', 'weit'));
     $object->setTarget(option('kirby3-dejure.target', '_blank'));
-    $object->setClass(option('kirby3-dejure.class', ''));
-    $object->setBuzer(option('kirby3-dejure.buzer', true));
-    $object->setCacheDuration(option('kirby3-dejure.cachePeriod', 2));
-    $object->setTimeout(option('kirby3-dejure.timeout', 3));
+    $object->setTooltip(option('kirby3-dejure.tooltip', 'neutral'));
 
-    return $object->dejurify($text);
+    # (3) Connection
+    $object->setStreamTimeout(option('kirby3-dejure.streamTimeout', 10));
+    $object->setTimeout(option('kirby3-dejure.timeout', 3));
+    $object->setUserAgent(option('kirby3-dejure.userAgent', 'kirby3-dejure @ ' . Kirby\Http\Server::host()));
+
+    # (4) Caching
+    $object->setCacheDuration(option('kirby3-dejure.cachePeriod', 2));
+
+    return $object->dejurify($text, $ignore);
+}
+
+
+/**
+ * Clears DJO cache
+ *
+ * @return bool Whether cache was cleared
+ */
+function clearDJO(): bool
+{
+    # Create DJO instance
+    $object = dejureInit();
+
+    # Clear cache
+    return $object->clearCache();
 }
 
 
@@ -49,23 +93,23 @@ Kirby::plugin('s1syphos/kirby3-dejure', [
     'hooks' => [
         'kirbytext:after' => function (string $text): string
         {
-            return dejurify($text);
-        }
+            return dejurify($text, option('kirby3-dejure.ignore', ''));
+        },
     ],
     'pageMethods' => [
-        'dejurify' => function (string $text): string
+        'dejurify' => function (string $text, string $ignore = ''): string
         {
-            return dejurify($text);
-        }
+            return dejurify($text, $ignore);
+        },
     ],
     'fieldMethods' => [
-        'dejurify' => function (Kirby\Cms\Field $field, bool $useKirbytext = true): string
+        'dejurify' => function (Kirby\Cms\Field $field, string $ignore = '', bool $useKirbytext = true): string
         {
             if ($useKirbytext === true) {
-                return dejurify($field->kt());
+                return dejurify($field->kt(), $ignore);
             }
 
-            return dejurify($field);
-        }
+            return dejurify($field, $ignore);
+        },
     ],
 ]);
--- vendor/composer/InstalledVersions.php
+++ vendor/composer/InstalledVersions.php
@@ -25,8 +25,8 @@ class InstalledVersions
     private static $installed = array (
   'root' => 
   array (
-    'pretty_version' => '1.1.0',
-    'version' => '1.1.0.0',
+    'pretty_version' => '1.2.0',
+    'version' => '1.2.0.0',
     'aliases' => 
     array (
     ),
@@ -89,6 +89,33 @@ class InstalledVersions
       ),
       'reference' => 'c98ece30bfba45be7ce457e1102d1b169d922f3d',
     ),
+    'guzzlehttp/guzzle' => 
+    array (
+      'pretty_version' => '7.3.0',
+      'version' => '7.3.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '7008573787b430c1c1f650e3722d9bba59967628',
+    ),
+    'guzzlehttp/promises' => 
+    array (
+      'pretty_version' => '1.4.1',
+      'version' => '1.4.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8e7d04f1f6450fef59366c399cfad4b9383aa30d',
+    ),
+    'guzzlehttp/psr7' => 
+    array (
+      'pretty_version' => '1.8.2',
+      'version' => '1.8.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'dc960a912984efb74d0a90222870c72c87f10c91',
+    ),
     'php-cs-fixer/diff' => 
     array (
       'pretty_version' => 'v1.3.1',
@@ -132,6 +159,38 @@ class InstalledVersions
         0 => '1.0',
       ),
     ),
+    'psr/http-client' => 
+    array (
+      'pretty_version' => '1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '2dfb5f6c5eff0e91e20e913f8c5452ed95b86621',
+    ),
+    'psr/http-client-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '1.0',
+      ),
+    ),
+    'psr/http-message' => 
+    array (
+      'pretty_version' => '1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f6561bf28d520154e4b0ec72be95418abe6d9363',
+    ),
+    'psr/http-message-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '1.0',
+      ),
+    ),
     'psr/log' => 
     array (
       'pretty_version' => '1.1.4',
@@ -157,10 +216,19 @@ class InstalledVersions
       ),
       'reference' => '408d5eafb83c57f6365a3ca330ff23aa4a5fa39b',
     ),
+    'ralouphie/getallheaders' => 
+    array (
+      'pretty_version' => '3.0.3',
+      'version' => '3.0.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '120b605dfeb996808c31b6477290a714d356e822',
+    ),
     's1syphos/kirby3-dejure' => 
     array (
-      'pretty_version' => '1.1.0',
-      'version' => '1.1.0.0',
+      'pretty_version' => '1.2.0',
+      'version' => '1.2.0.0',
       'aliases' => 
       array (
       ),
@@ -168,12 +236,12 @@ class InstalledVersions
     ),
     's1syphos/php-dejure' => 
     array (
-      'pretty_version' => '1.2.0',
-      'version' => '1.2.0.0',
+      'pretty_version' => '1.3.2',
+      'version' => '1.3.2.0',
       'aliases' => 
       array (
       ),
-      'reference' => '60e57083f21a9da99026a972f8088e21b8908703',
+      'reference' => 'e04f36332b4542b0c45dc18b46b2483ec961c233',
     ),
     'shieldon/simple-cache' => 
     array (
