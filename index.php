c246d0c644cdfc0f148d2164b15ff0f185c9d84e
Martin Folkers <hello@twobrain.io>
2021-06-25 08:54:21 +0200

    Implement new parameters & rework setters

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 use S1SYPHOS\DejureOnline;
 
 
-function dejurify(string $text): string
+function dejurify(string $text, string $ignore = ''): string
 {
     # Leave text unmodified if plugin is disabled (default)
     if (!option('kirby3-dejure.enabled', false)) {
@@ -26,22 +26,33 @@ function dejurify(string $text): string
     $cacheDir = kirby()->root('cache') . '/dejure-online.org/';
 
     # Initialize DJO instance
-    $object = new DejureOnline($cacheDir,
+    $object = new DejureOnline(
         option('kirby3-dejure.driver', 'file'),
-        option('kirby3-dejure.caching')
+        option('kirby3-dejure.caching', ['storage' => $cacheDir])
     );
 
     # Set defaults
-    $object->setProvider(option('kirby3-dejure.provider', Kirby\Http\Server::host()));
-    $object->setMail(option('kirby3-dejure.mail', ''));
+
+    # (1) General information
+    $object->setEmail(option('kirby3-dejure.mail', ''));
+
+    # (2) Text processing
+    $object->setBuzer(option('kirby3-dejure.buzer', true));
+    $object->setClass(option('kirby3-dejure.class', ''));
+    $object->setLineBreak(option('kirby3-dejure.lineBreak', 'auto'));
     $object->setLinkStyle(option('kirby3-dejure.linkStyle', 'weit'));
     $object->setTarget(option('kirby3-dejure.target', '_blank'));
-    $object->setClass(option('kirby3-dejure.class', ''));
-    $object->setBuzer(option('kirby3-dejure.buzer', true));
-    $object->setCacheDuration(option('kirby3-dejure.cachePeriod', 2));
-    $object->setTimeout(option('kirby3-dejure.timeout', 3));
+    $object->setTooltip(option('kirby3-dejure.tooltip', 'neutral'));
 
-    return $object->dejurify($text);
+    # (3) Connection
+    $object->setStreamTimeout(option('kirby3-dejure.streamTimeout', 10));
+    $object->setTimeout(option('kirby3-dejure.timeout', 3));
+    $object->setUserAgent(option('kirby3-dejure.userAgent', 'kirby3-dejure @ ' . Kirby\Http\Server::host()));
+
+    # (4) Caching
+    $object->setCacheDuration(option('kirby3-dejure.cachePeriod', 2));
+
+    return $object->dejurify($text, $ignore);
 }
 
 
@@ -49,23 +60,23 @@ Kirby::plugin('s1syphos/kirby3-dejure', [
     'hooks' => [
         'kirbytext:after' => function (string $text): string
         {
-            return dejurify($text);
+            return dejurify($text, option('kirby3-dejure.ignore', ''));
         }
     ],
     'pageMethods' => [
-        'dejurify' => function (string $text): string
+        'dejurify' => function (string $text, string $ignore = ''): string
         {
-            return dejurify($text);
+            return dejurify($text, $ignore);
         }
     ],
     'fieldMethods' => [
-        'dejurify' => function (Kirby\Cms\Field $field, bool $useKirbytext = true): string
+        'dejurify' => function (Kirby\Cms\Field $field, string $ignore = '', bool $useKirbytext = true): string
         {
             if ($useKirbytext === true) {
-                return dejurify($field->kt());
+                return dejurify($field->kt(), $ignore);
             }
 
-            return dejurify($field);
+            return dejurify($field, $ignore);
         }
     ],
 ]);
